7573e5473246d9f3a2f0ff1da2bed96fbcb1d95b
NVadimD <vadim.zelep@gmail.com>
2023-06-15 14:38:54 +0700

    Чуть подебажил

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ const shouldDoList = document.querySelector('.shouldDo__list');
 const haveDoneList = document.querySelector('.haveDone__list');
 
 
+
+
 const model = {
     tasks: [],
+
+    renderTasks: function() {
+        const shouldDoTasks = this.tasks.filter(item => item.hasComplete === false);
+        shouldDoTasks.forEach(function(item) {
+            const newTask = document.createElement('li');
+            newTask.textContent = item.text;
+            shouldDoList.appendChild(newTask);
+        });
+        const haveDoneTasks = this.tasks.filter(item => item.hasComplete === true);
+        haveDoneTasks.forEach(function(item) {
+            const newTask = document.createElement('li');
+            newTask.textContent = item.text;
+            haveDoneList.appendChild(newTask);
+        });
+    },
 
     checkValue: function() {
         if (textInput.value === '') {
@@ -24,9 +41,9 @@ const model = {
             id: new Date().getTime(),
             text: textInput.value,
             hasComplete: false
-        };
-        this.idNumber++;
+        };        
         model.tasks.push(newTask);
+        localStorage.setItem('tasks', JSON.stringify(model.tasks));
 
         const task = document.createElement('li');
         task.textContent = newTask.text;
@@ -39,14 +56,27 @@ const model = {
         shouldDoList.removeChild(event.target);
         haveDoneList.appendChild(event.target);
         const ind = model.tasks.findIndex(elem => elem.text === event.target.textContent);
-        model.tasks[ind].hasComplete = true;     
+        model.tasks[ind].hasComplete = true;
+        localStorage.setItem('tasks', JSON.stringify(model.tasks));     
     },
 
     removeTask: function(event) {
         haveDoneList.removeChild(event.target);
         const ind = this.tasks.findIndex(elem => elem.text === event.target.textContent);
         this.tasks.splice(ind, 1);
+        localStorage.setItem('tasks', JSON.stringify(model.tasks));
     }
+}
+
+
+
+// --------------------------------Проверка LocalStorage и отрисовка задач--------------------------------
+
+
+
+if (localStorage.getItem) {
+    model.tasks = JSON.parse(localStorage.getItem('tasks'));
+    model.renderTasks();
 }
 
 
@@ -55,6 +85,11 @@ const model = {
 
 
 
+
+
+
+
+// ------------------------------------Слушатели и запуск работы по задачам-----------------------------------
 
 btn.addEventListener('click', model.addTask.bind(model));
 
@@ -77,3 +112,9 @@ shouldDoList.addEventListener('click', function(event) {
 haveDoneList.addEventListener('click', function(event) {
     model.removeTask(event);
 })
+
+
+
+
+
+
